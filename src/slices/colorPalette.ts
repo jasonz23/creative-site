b5b22788b71d173e25776da29ae017bba908d3fe
Jason Zhao <jason1382311@gmail.com>
2023-02-22 23:53:29 -0800

    implment set color palette, swap colors and set color

--- src/slices/colorPalette.ts
+++ src/slices/colorPalette.ts
@@ -2,24 +2,73 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface ColorPaletteState {
-    colorPalette: number[][],
+    colorPalette: string[],
+}
+
+interface SwapColorPalettePayloadState {
+    index: number,
+    swap: boolean
+}
+
+interface APIColorPaletteState {
+    result: number[][]
+}
+
+interface SetColorPayloadState {
+    index: number,
+    color: string
 }
 
 const initialState: ColorPaletteState = {
     colorPalette: [],
-
 }
 
 const slice = createSlice({
     name: "colorPalette",
     initialState,
     reducers:{
-        setColorPalette: (state: ColorPaletteState, {payload}: PayloadAction<number[][]>)=> {
-            state.colorPalette = payload;
+        setColorPalette: (state: ColorPaletteState, {payload}: PayloadAction<APIColorPaletteState>) => {
+            const newPayload:string[] = payload.result?.map((color) => {
+                let hexString = '#';
+                color.forEach((c) => {
+                    var hex = c.toString(16);
+                    hexString += hex.length == 1 ? "0" + hex : hex;
+                })
+                return hexString;
+            })
+            state.colorPalette = newPayload;
+        },
+        swapColorPaletteIndex: (state: ColorPaletteState, {payload}: PayloadAction<SwapColorPalettePayloadState>) => {
+            let colorPaletteCopy = [...state.colorPalette];
+            if (payload.swap) {
+                if (payload.index != 4) {
+                    const temp = colorPaletteCopy[payload.index]
+                    colorPaletteCopy[payload.index] = colorPaletteCopy[payload.index + 1]
+                    colorPaletteCopy[payload.index + 1] = temp
+                } else {
+                    const colorElement = colorPaletteCopy.pop()!;
+                    colorPaletteCopy.unshift(colorElement);
+                }
+            } else {
+                if (payload.index != 0) {
+                    const temp = colorPaletteCopy[payload.index]
+                    colorPaletteCopy[payload.index] = colorPaletteCopy[payload.index - 1]
+                    colorPaletteCopy[payload.index - 1] = temp
+                } else {
+                    const colorElement = colorPaletteCopy[0];
+                    colorPaletteCopy = colorPaletteCopy.splice(1,colorPaletteCopy.length);
+                    colorPaletteCopy.push(colorElement);
+                }
+            }
+            console.log(colorPaletteCopy)
+            state.colorPalette = colorPaletteCopy;
+        },
+        setColor: (state: ColorPaletteState, {payload}: PayloadAction<SetColorPayloadState>) => {
+            state.colorPalette[payload.index] = payload.color;
         }
     }
 })
 
-export const {setColorPalette} = slice.actions;
+export const {setColorPalette, swapColorPaletteIndex, setColor} = slice.actions;
 
 export const colorPalette = slice.reducer;
