f00e6c1722a1f1f89c756d62d599ee8d87ba5bec
Jason Zhao <jason1382311@gmail.com>
2023-02-22 23:51:59 -0800

    Add nav-bar, loading icon and color container and button to re generate colors

---
+++ src/pages/main-page/MainPage.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@mui/material";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getColorPalette } from "../../api/colorMind";
+import ColorContainer from "../../components/color-container/ColorContainer";
+import LoadingIcon from "../../components/loading-icon/LoadingIcon";
+import NavBar from "../../components/nav-bar/NavBar";
+import { useAppDispatch, useAppSelector } from "../../slices";
+
+const MainPage = () => {
+  const dispatch = useAppDispatch();
+  const colorPalette = useAppSelector(
+    (state) => state?.colorPalette?.colorPalette
+  );
+  useEffect(() => {
+    console.log(colorPalette);
+  });
+  return (
+    <>
+      <NavBar />
+      <LoadingIcon />
+      <div
+        style={{
+          backgroundColor: colorPalette[3],
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1 style={{ color: colorPalette[0] }}>Color Palette Generator</h1>
+        <ColorContainer />
+        <Button
+          variant="contained"
+          style={{
+            marginTop: "30px",
+            backgroundColor: "black",
+          }}
+          onClick={() => {
+            dispatch(getColorPalette());
+          }}
+        >
+          Generate
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
