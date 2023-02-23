05feb077bc3e50d72dd1b0679a4d1af82f6725fe
Jason Zhao <jason1382311@gmail.com>
2023-02-22 23:51:07 -0800

    implement loading icon with redux state to know when to show:

---
+++ src/components/loading-icon/LoadingIcon.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useAppSelector } from "../../slices";
+import "./LoadingIcon.css";
+
+const LoadingIcon = () => {
+  const showLoadingIcon = useAppSelector(
+    (state) => state?.notification?.loadingIcon
+  );
+  useEffect(() => {
+    console.log(showLoadingIcon);
+  });
+  return showLoadingIcon ? (
+    <div className="loading-icon-root">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default LoadingIcon;
