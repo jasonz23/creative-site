24ef56940efa6de87ac545a979b65203cb4522e5
Jason Zhao <jason1382311@gmail.com>
2023-02-22 23:52:57 -0800

    change route for main page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import MainPage from "./mainpage/MainPage";
+import MainPage from "./main-page/MainPage";
 import { getColorPalette } from "../api/colorMind";
 import { useAppDispatch } from "../slices";
 
@@ -10,6 +10,7 @@ function App() {
   useEffect(() => {
     dispatch(getColorPalette());
   }, []);
+
   return (
     <>
       <HashRouter>
