2e0a0d84197645991442b7cf2fe0b250a29fdfb9
Jason Zhao <jason1382311@gmail.com>
2023-02-25 17:35:17 -0800

    add title to nav bar

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import AppBar from "@mui/material/AppBar";
 import { useAppSelector } from "../../slices";
 
@@ -11,8 +10,25 @@ const NavBar = (): JSX.Element => {
       position="fixed"
       style={{ height: "50px", backgroundColor: colorPalette[2] }}
     >
-      <div style={{ fontSize: "10px" }}>Color 3</div>
-      <div style={{ display: "flex" }}></div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div
+          style={{
+            marginLeft: "20px",
+            color: colorPalette[0],
+            fontWeight: "bold",
+          }}
+        >
+          Color Generator Site
+        </div>
+      </div>
     </AppBar>
   );
 };
