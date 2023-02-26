a6617c9560131685e7be58511c21d65d84657205
Jason Zhao <jason1382311@gmail.com>
2023-02-25 17:34:32 -0800

    add props to color container which holds color palette and if its main, remove height set on non-main

--- src/components/color-container/ColorContainer.tsx
+++ src/components/color-container/ColorContainer.tsx
@@ -1,14 +1,23 @@
 import { useAppSelector } from "../../slices";
 import ColorDisplay from "../color-display/ColorDisplay";
 import "./ColorContainer.css";
-const ColorContainer = () => {
+interface PropsState {
+  colorPalette: string[];
+  main: boolean;
+}
+const ColorContainer = (props: PropsState) => {
   const colorPalette = useAppSelector(
     (state) => state?.colorPalette?.colorPalette
   );
   return (
-    <div className="color-container-root">
-      {colorPalette.map((color, index) => {
-        return <ColorDisplay id={index.toString()} color={color} />;
+    <div
+      className="color-container-root"
+      style={{ height: props.main ? "250px" : "auto" }}
+    >
+      {props.colorPalette.map((color, index) => {
+        return (
+          <ColorDisplay id={index.toString()} color={color} main={props.main} />
+        );
       })}
     </div>
   );
