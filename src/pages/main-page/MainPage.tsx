55b4714e571ef641e36a21f7fae5af191a1f84b6
Jason Zhao <jason1382311@gmail.com>
2023-02-25 17:35:35 -0800

    remove main page content on loading

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -12,7 +12,7 @@ import ColorContainer from "../../components/color-container/ColorContainer";
 import LoadingIcon from "../../components/loading-icon/LoadingIcon";
 import NavBar from "../../components/nav-bar/NavBar";
 import { useAppDispatch, useAppSelector } from "../../slices";
-import { setRandomColor } from "../../slices/colorPalette";
+import { addToPrevious, getRandomColor } from "../../slices/colorPalette";
 import { setErrorMessage } from "../../slices/notifications";
 import "./MainPage.css";
 const MainPage = () => {
@@ -26,6 +26,10 @@ const MainPage = () => {
   const errorMessage = useAppSelector(
     (state) => state?.notification?.errorMessage
   );
+  const previous = useAppSelector((state) => state?.colorPalette?.previous);
+  const loadingState = useAppSelector(
+    (state) => state?.notification?.loadingIcon
+  );
   const [modeInput, setModeInput] = useState(null);
   const ref = useRef<any>(null);
 
@@ -33,113 +37,140 @@ const MainPage = () => {
     <>
       <NavBar />
       <LoadingIcon />
-      <div
-        style={{
-          backgroundColor: colorPalette[3],
-          height: "100vh",
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          position: "fixed",
-        }}
-      >
-        <h1 style={{ color: colorPalette[0] }}>Color Palette Generator</h1>
-        <ColorContainer />
-
-        {errorMessage !== "" ? (
-          <div
-            id="error-message"
-            className="color-error-body"
-            style={{ color: colorPalette[0], backgroundColor: colorPalette[4] }}
-          >
-            {errorMessage}
-          </div>
-        ) : null}
-        <form
-          className="color-palette-selector-form"
-          ref={ref}
-          style={{ backgroundColor: colorPalette[4] }}
+      {!loadingState ? (
+        <div
+          style={{
+            backgroundColor: colorPalette[3],
+            marginTop: "50px",
+            minHeight: "100vh",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            overflow: "auto",
+          }}
         >
-          <TextField
-            id="hexcode"
-            label="Hex Code"
-            variant="outlined"
-            style={{ marginRight: "10px", backgroundColor: "white" }}
-          />
-          <FormControl style={{ minWidth: 120 }}>
-            <InputLabel>Mode</InputLabel>
-            <Select
-              value={modeInput}
-              placeholder="Mode"
-              onChange={(e: any) => {
-                setModeInput(e.target.value);
+          <h1 style={{ color: colorPalette[0] }}>Color Palette Generator</h1>
+          <ColorContainer colorPalette={colorPalette} main={true} />
+
+          {errorMessage !== "" ? (
+            <div
+              id="error-message"
+              className="color-error-body"
+              style={{
+                color: colorPalette[0],
+                backgroundColor: colorPalette[4],
               }}
-              style={{ marginLeft: "10px", backgroundColor: "white" }}
             >
-              <MenuItem value="monochrome">Monochrome</MenuItem>
-              <MenuItem value="monochrome-dark">Monochrome Dark</MenuItem>
-              <MenuItem value="monochrome-light">Monochrome Light</MenuItem>
-              <MenuItem value="analogic">Analogic</MenuItem>
-              <MenuItem value="complement">Complement</MenuItem>
-              <MenuItem value="analogic-complement">
-                Analogic Complement
-              </MenuItem>
-              <MenuItem value="triad">Triad</MenuItem>
-              <MenuItem value="quad">Quad</MenuItem>
-            </Select>
-          </FormControl>
-        </form>
-        <div>
-          <Button
-            variant="contained"
-            style={{
-              marginTop: "30px",
-              backgroundColor: "black",
-              marginRight: "10px",
-            }}
-            onClick={() => {
-              const hex = ref?.current?.elements["hexcode"].value.replace(
-                "#",
-                ""
-              );
-              if (hex.length === 6 && /^#[0-9A-F]{6}$/i.test(hex)) {
+              {errorMessage}
+            </div>
+          ) : null}
+          <form
+            className="color-palette-selector-form"
+            ref={ref}
+            style={{ backgroundColor: colorPalette[4] }}
+          >
+            <TextField
+              id="hexcode"
+              label="Hex Code"
+              variant="outlined"
+              style={{ marginRight: "10px", backgroundColor: "white" }}
+            />
+            <FormControl style={{ minWidth: 120 }}>
+              <InputLabel>Mode</InputLabel>
+              <Select
+                value={modeInput}
+                placeholder="Mode"
+                onChange={(e: any) => {
+                  setModeInput(e.target.value);
+                }}
+                style={{ marginLeft: "10px", backgroundColor: "white" }}
+              >
+                <MenuItem value="monochrome">Monochrome</MenuItem>
+                <MenuItem value="monochrome-dark">Monochrome Dark</MenuItem>
+                <MenuItem value="monochrome-light">Monochrome Light</MenuItem>
+                <MenuItem value="analogic">Analogic</MenuItem>
+                <MenuItem value="complement">Complement</MenuItem>
+                <MenuItem value="analogic-complement">
+                  Analogic Complement
+                </MenuItem>
+                <MenuItem value="triad">Triad</MenuItem>
+                <MenuItem value="quad">Quad</MenuItem>
+              </Select>
+            </FormControl>
+          </form>
+          <div>
+            <Button
+              variant="contained"
+              style={{
+                marginTop: "30px",
+                backgroundColor: "black",
+                marginRight: "10px",
+              }}
+              onClick={() => {
+                const hex = ref?.current?.elements["hexcode"].value.replace(
+                  "#",
+                  ""
+                );
+                if (
+                  hex.length === 6 &&
+                  /^#?([a-f0-9]{6}|[a-f0-9]{3})$/.test(hex)
+                ) {
+                  dispatch(addToPrevious(colorPalette));
+                  dispatch(
+                    getColorPalette({
+                      hexcode: hex,
+                      mode: modeInput ?? "monochrome",
+                    })
+                  );
+                  dispatch(setErrorMessage(""));
+                } else {
+                  dispatch(
+                    setErrorMessage("Please enter 6 character hex code")
+                  );
+                }
+              }}
+            >
+              Generate
+            </Button>
+            <Button
+              variant="contained"
+              style={{
+                marginTop: "30px",
+                backgroundColor: "black",
+                marginLeft: "10px",
+              }}
+              onClick={() => {
+                dispatch(addToPrevious(colorPalette));
                 dispatch(
                   getColorPalette({
-                    hexcode: hex,
+                    hexcode: getRandomColor(),
                     mode: modeInput ?? "monochrome",
                   })
                 );
-                dispatch(setErrorMessage(""));
-              } else {
-                dispatch(setErrorMessage("Please enter 6 character hex code"));
-              }
-            }}
-          >
-            Generate
-          </Button>
-          <Button
-            variant="contained"
+              }}
+            >
+              Generate Random
+            </Button>
+          </div>
+          <div
             style={{
-              marginTop: "30px",
-              backgroundColor: "black",
-              marginLeft: "10px",
-            }}
-            onClick={() => {
-              dispatch(setRandomColor());
-              dispatch(
-                getColorPalette({
-                  hexcode: hexcode,
-                  mode: modeInput ?? "monochrome",
-                })
-              );
+              marginTop: "10px",
+              fontWeight: "bold",
+              fontSize: "20px",
+              color: colorPalette[0],
             }}
           >
-            Generate Random
-          </Button>
+            Previous Color Palettes
+          </div>
+          {previous.map((colors) => {
+            return <ColorContainer colorPalette={colors} main={false} />;
+          })}
+          {previous.length !== 0 ? (
+            <div style={{ marginTop: "30px" }}></div>
+          ) : null}
         </div>
-      </div>
+      ) : null}
     </>
   );
 };
